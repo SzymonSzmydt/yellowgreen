64223773ee3449e5bb4cb7036c424ce0b6866164
SzymonSzmydt <szymonszmydt@gmail.com>
2023-02-23 10:47:19 +0100

    add Redux toolkit state manegament

---
+++ context/redux/productsSlice.tsx
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../../app/store';
+import { CorrectProductType } from '../../components/dashboard/types/type';
+
+interface ProductsState {
+  value: Array<CorrectProductType>;
+}
+
+const initialState: ProductsState = {
+  value: [],
+};
+
+export const productsSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {
+    getProducts: (state, action: PayloadAction<Array<CorrectProductType>>) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { getProducts } = productsSlice.actions;
+
+export const selectProducts = (state: RootState) => state.products.value;
+export default productsSlice.reducer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import { Inter } from '@next/font/google';
 import Layout from '../components//layout/Layout';
+import { Provider } from 'react-redux';
+import { store } from '../context/redux/store';
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -17,7 +19,12 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
-    Component.getLayout ?? ((page) => <Layout> {page} </Layout>);
+    Component.getLayout ??
+    ((page) => (
+      <Provider store={store}>
+        <Layout> {page} </Layout>
+      </Provider>
+    ));
 
   return getLayout(
     <>
@@ -26,7 +33,9 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
           font-family: ${inter.style.fontFamily};
         }
       `}</style>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 }
