cc399632da2488f3b12ef0663dfbfc0b8d5ab7c4
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-02 17:34:16 +0100

    add meta tags

--- pages/products/[productId].tsx
+++ pages/products/[productId].tsx
@@ -2,6 +2,7 @@ import style from './product.module.css';
 import type { CorrectProductType } from './../../components/dashboard/types/type';
 import { BasketCalc } from './../../components/products/basketCalc';
 import { Image } from 'next/image';
+import { Head } from 'next/document';
 
 type ProductProps = {
   product: CorrectProductType;
@@ -9,10 +10,26 @@ type ProductProps = {
 
 function Product({ product }: ProductProps) {
   return (
-    <div className={style.product}>
-      <BasketCalc product={product} />
-      <div className={style.imageBox}></div>
-    </div>
+    <>
+      <Head>
+        <meta name="description" content={`${product.namePL}`} />
+        <meta name="description" content={`${product.descriptionPL}`} />
+        <title> {product.namePL}</title>
+      </Head>
+      <div className={style.product}>
+        <section className={style.basket}>
+          <BasketCalc product={product} />
+          <div className={style.imageBox}>
+            <em>Zdjęcie produktu</em>
+          </div>
+        </section>
+        <section>
+          <p>Kategoria</p>
+          <p>{product.category}</p>
+          <p> {product.descriptionPL}</p>
+        </section>
+      </div>
+    </>
   );
 }
 
