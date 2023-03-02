c36144b488526857ae1cecc1f4662d990dcfc570
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-02 12:24:30 +0100

    update file paths

--- components/body/commercial.tsx
+++ components/body/commercial.tsx
@@ -1,35 +1,35 @@
-import style from "./style.module.css";
-import Image from "next/image";
-import Link from "next/link";
+import style from './style.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Commercial() {
   return (
     <main className={style.main}>
-      <Link href='/product/1'>
+      <Link href="/products/1">
         <Image
           src={`/1.webp`}
-          alt='first'
+          alt="first"
           className={style.img}
-          width='240'
-          height='320'
+          width="240"
+          height="320"
         />
       </Link>
-      <Link href='/product/2'>
+      <Link href="/products/2">
         <Image
           src={`/2.webp`}
-          alt='second'
+          alt="second"
           className={style.img}
-          width='240'
-          height='320'
+          width="240"
+          height="320"
         />
       </Link>
-      <Link href='/product/3'>
+      <Link href="/products/3">
         <Image
           src={`/3.webp`}
-          alt='first'
+          alt="first"
           className={style.img}
-          width='240'
-          height='320'
+          width="240"
+          height="320"
         />
       </Link>
     </main>
