c53e8c3653782af473096a1bd0142d3d4ed6cdfa
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-03 18:22:43 +0100

    and small navigation links

--- components/header/navigation.tsx
+++ components/header/navigation.tsx
@@ -1,27 +1,50 @@
-import style from "./style.module.css";
-import Link from "next/link";
+import style from './style.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Navigation() {
+  const router = useRouter();
+  console.log(router);
+
+  const paths: string[] = router?.asPath.split('/').filter((e) => e);
   return (
-    <nav className={style.nav}>
-      <div className={style.navBox}>
-        <Link href='/furniture' className={style.link}>
-          Meble
+    <>
+      <nav className={style.nav}>
+        <div className={style.navBox}>
+          <Link href="/furniture" className={style.link}>
+            Meble
+          </Link>
+          <Link href="/lamps" className={style.link}>
+            Lampy
+          </Link>
+          <Link href="/accesories" className={style.link}>
+            Akcesoria
+          </Link>
+          <Link href="/promotions" className={style.link}>
+            Promocje
+          </Link>
+          <Link href="/contact" className={style.link}>
+            Kontakt
+          </Link>
+        </div>
+      </nav>
+      <div className={style.smallNav}>
+        <Link key="home" href="/" className={style.smallLink}>
+          Home
         </Link>
-        <Link href='/lamps' className={style.link}>
-          Lampy
-        </Link>
-        <Link href='/accesories' className={style.link}>
-          Akcesoria
-        </Link>
-        <Link href='/promotions' className={style.link}>
-          Promocje
-        </Link>
-        <Link href='/contact' className={style.link}>
-          Kontakt
-        </Link>
+        {paths
+          ? paths.map((link, index) => (
+              <Link
+                key={link}
+                href={index === 0 ? `/${link}` : link}
+                className={style.smallLink}
+              >
+                {link}
+              </Link>
+            ))
+          : null}
       </div>
-    </nav>
+    </>
   );
 }
 
