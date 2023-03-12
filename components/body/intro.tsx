5a4245bfc31b77fb347bef0eedd65cf392075a28
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-12 21:24:59 +0100

    created Intro component

--- components/body/intro.tsx
+++ components/body/intro.tsx
@@ -1,11 +1,39 @@
 import style from './styles/intro.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 type IntroProps = {
   products: CorrectProductType[];
 };
 
 function Intro({ products }: IntroProps) {
-  return <section className={style.intro}></section>;
+  const product = products.find((product) => product.id === 1678469036739);
+  return (
+    <section className={style.intro}>
+      <div className={style.title}>
+        <h2>{product.category}</h2>
+      </div>
+      <div className={style.box}>
+        <Link href="/garderoba">
+          <Image
+            src={product.image2}
+            width={600}
+            height={600}
+            alt={product.namePL}
+          />
+        </Link>
+        <div className={style.text}>
+          <h3>Subtelne spojrzenie</h3>
+          <p>
+            Uporządkowana biżuteria, zegarki czy paski&nbsp;w szufladzie daje
+            absolutne poczucie komfortu i estetyki. Z&nbsp;wkładami
+            garderobianymi o niezwykle pięknym wykończeniu harmonia wnętrza się
+            dopełnia.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Intro;
