36e4ab7ed83604e85a9622009185f80cdf590679
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-17 14:23:53 +0100

    add basket value calculation

--- components/basket/summary.tsx
+++ components/basket/summary.tsx
@@ -9,24 +9,28 @@ type SummaryProps = {
 export function Summary({ basket }: SummaryProps) {
   const totalProductValue = basket
     .map((product) => product.quantity * product.price)
-        .reduce((a, b) => a + b, 0);
-    
-    const deliveryPrice = basket
-      .map((product) => product.quantity * product.price)
-      .reduce((a, b) => a + b, 0);
+    .reduce((a, b) => a + b, 0);
+  const totalProductPrice: number = parseFloat(totalProductValue.toFixed(2));
+
+  const deliveryPrice = basket
+    .map((product) => product.quantity * product.delivery)
+    .reduce((a, b) => a + b, 0);
+  const totalDeliceryPrice: number = parseFloat(deliveryPrice.toFixed(2));
 
   return (
     <section className={style.summary}>
       <h2>Podsumowanie zamówienia</h2>
       <div>
-        <span>Wartość zamówienia</span>{' '}
-        {parseFloat(totalProductValue.toFixed(2))} zł
+        <span>Wartość zamówienia</span>
+        {totalProductPrice} zł
       </div>
       <div>
         <span>Opłaty za wysyłkę</span>
+        {totalDeliceryPrice} zł
       </div>
       <strong>
         <span>Do zapłaty</span>
+        {totalProductPrice + totalDeliceryPrice} zł
       </strong>
       <Standard name={'Do kasy'} handleClick={() => ''} />
     </section>
