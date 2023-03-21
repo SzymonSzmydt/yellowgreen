c88359087bb6e3d270389bc7f7613a3d8425f3e9
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-21 17:32:28 +0100

    add visible password option

--- pages/dashboard/login.tsx
+++ pages/dashboard/login.tsx
@@ -2,9 +2,18 @@ import { Standard } from 'components/button/standard';
 import style from './styles/login.module.css';
 import { useState } from 'react';
 import type { ReactElement } from 'react';
+import eyeIconOn from '../../public/icons/pass/visibility_on.svg';
+import eyeIconOff from '../../public/icons/pass/visibility_off.svg';
+import Image from 'next/image';
+
+type LoginProps = {
+  login: string;
+  password: string;
+};
 
 export default function Login() {
-  const [user, setUser] = useState({ login: '', password: '' });
+  const [user, setUser] = useState<LoginProps>({ login: '', password: '' });
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -12,6 +21,7 @@ export default function Login() {
 
   return (
     <div className={style.login}>
+      <h2>Wymagana autoryzacja</h2>
       <form onSubmit={handleSubmit} className={style.form}>
         <label>Login</label>
         <input
@@ -26,17 +36,26 @@ export default function Login() {
           }
         />
         <label>Hasło</label>
-        <input
-          type="password"
-          name="password"
-          className={style.input}
-          autoComplete="on"
-          required
-          min={4}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setUser({ ...user, password: e.target.value })
-          }
-        />
+        <section className={style.pass}>
+          <input
+            type={isVisible ? 'text' : 'password'}
+            name="password"
+            className={style.input}
+            autoComplete="on"
+            required
+            min={4}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUser({ ...user, password: e.target.value })
+            }
+          />
+          <Image
+            src={isVisible ? eyeIconOn : eyeIconOff}
+            onClick={() => setIsVisible((state) => !state)}
+            className={style.visibility}
+            alt="Password is hidden"
+          />
+        </section>
+
         <div className={style.btn}>
           <Standard name="Zaloguj" handleClick={() => null} />
         </div>
