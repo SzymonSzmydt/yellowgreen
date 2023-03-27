d96f1534dc79890ca4dba7884ef38e15864b6c8b
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-27 15:22:21 +0200

    fix RWD

---
+++ components/products/productId/productDetailist.tsx
@@ -0,0 +1,33 @@
+import style from './styles/detaillist.module.css';
+
+export function ProductDetaillist() {
+  return (
+    <>
+      <details className={style.details}>
+        <summary>Sposoby Płatności</summary>
+        <br />
+        <h4>Obsługujemy wygodne płatności</h4>
+        <p>
+          Za wszystkie produkty zapłacisz wygodnie, BLIK&apos;iem, Kartą
+          płatniczą, Kartą debetową.
+        </p>
+      </details>
+      <details className={style.details}>
+        <summary>Wysyłka i Zwrot</summary>
+        <p>Czas dostawy: 3 - 5 dni roboczych</p>
+        <h4>Wysyłka</h4>
+        <p>
+          Wszystkie artykuły zostaną dostarczone&nbsp;do Twojego domu lub
+          paczkomatu, który wybierzesz.
+        </p>
+        <h4>Zwrot</h4>
+        <p>
+          Jeśli produkt nie spełni Twoich oczekiwań, możesz odesłać nam&nbsp;go
+          spowrotem. Przykładamy wielką wagę do jakości obsługi naszych
+          klientów&nbsp;i pragniemy, aby doświadczenia zakupowe były na
+          najwyższym poziomie!
+        </p>
+      </details>
+    </>
+  );
+}
--- components/products/productId/productId.tsx
+++ components/products/productId/productId.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { BasketCalc } from '../basketCalc';
 import Image from 'next/image';
 import { PorductDescription } from './productDescription';
+import { ProductDetaillist } from './productDetailist';
 
 type ProductProps = {
   product: CorrectProductType;
@@ -38,31 +39,7 @@ export function ProductId({ product }: ProductProps) {
         <section className={style.box}>
           <div className={style.basket}>
             <BasketCalc product={product} />
-            <details className={style.details}>
-              <summary>Sposoby Płatności</summary>
-              <br />
-              <h4>Obsługujemy wygodne płatności</h4>
-              <p>
-                Za wszystkie produkty zapłacisz wygodnie, BLIK&apos;iem, Kartą
-                płatniczą, Kartą debetową.
-              </p>
-            </details>
-            <details className={style.details}>
-              <summary>Wysyłka i Zwrot</summary>
-              <p>Czas dostawy: 3 - 5 dni roboczych</p>
-              <h4>Wysyłka</h4>
-              <p>
-                Wszystkie artykuły zostaną dostarczone&nbsp;do Twojego domu lub
-                paczkomatu, który wybierzesz.
-              </p>
-              <h4>Zwrot</h4>
-              <p>
-                Jeśli produkt nie spełni Twoich oczekiwań, możesz odesłać
-                nam&nbsp;go spowrotem. Przykładamy wielką wagę do jakości
-                obsługi naszych klientów&nbsp;i pragniemy, aby doświadczenia
-                zakupowe były na najwyższym poziomie!
-              </p>
-            </details>
+            <ProductDetaillist />
           </div>
           <div className={style.imageBox}>
             <figcaption className={style.figcaption}>
